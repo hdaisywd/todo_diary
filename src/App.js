2419cf9f1fcd122d1aa72cd0a1630701211b5016
JEONGYUNKIMm <0higherfurtherfaster0@sookmyung.ac.kr>
2023-01-13 16:12:24 +0900

    프로젝트 생성

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        지윤님 안녕하세욧 (,,・e・)pー━*＊※*⌒* 뭔가 바뀌면 바꼈다고 떠야 하는 거 아닌가?
+        
+      </p>
+    </div>
+  );
+}
+
+export default App;
