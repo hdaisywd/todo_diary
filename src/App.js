97866ca93632c7f5f8a481f9a0da6001b4a4220c
정지윤 <samtori3849@gmail.com>
2023-01-16 15:59:49 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
 import Home from './page/Home.js'
 import Button from 'react-bootstrap/Button';
+import loginBtn from './login.png';
+import logo from './logo.png';
+import { Toast } from 'bootstrap';
 
 function App() {
   return (
     <div className='App'>
       <div className='mainBox'>
-        <h1>😊</h1><br/><br/>
-        <p><h1>환영합니다!</h1></p><br/><br/>
-        <p><input type="text" placeholder='아이디를 입력하세요.'></input></p>
-        <p><input type="text" placeholder='비밀번호를 입력하세요.'></input></p>
+        <h1><img alt='logo' src={logo}/></h1><br/><br/>
+        <p><h1 className='welcome'>환영합니다!</h1></p><br/><br/>
+        <p><input type="text" placeholder='아이디를 입력하세요.'></input></p><br/>
+        <p><input type="password" placeholder='비밀번호를 입력하세요.'></input></p>
         <br/><br/>
-        <p><Button variant="info">LOGIN</Button></p>
+        <p><img alt="login" src={loginBtn}/></p>
         <text>회원가입</text>
       </div>
+      <text className='title'><h1>오늘의 투다</h1></text>
     </div>
   );
 }
