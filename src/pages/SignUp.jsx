eacd580455df68ff209c94b5d8a938e1168cf4b3
JEONGYUNKIMm <0higherfurtherfaster0@sookmyung.ac.kr>
2023-01-22 18:56:34 +0900

    회원가입 - css 수정

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import {useDispatch} from 'react-redux';
 import "./main.css";
+import regiserBtn from "./resources/회원가입.png";
+import logo from "./resources/로고.png";
 function SignUp(){
     //const dispatch = useDispatch();
 
@@ -27,10 +29,6 @@ function SignUp(){
             return alert('비밀번호를 다시 확인해주세요.');
         }
 
-        else {
-            return alert('환영합니다!');
-        }
-
         let body = {
             id: Id,
             password: Password,
@@ -42,17 +40,18 @@ function SignUp(){
     return (
         <div className='signup'>
             <div className="mainBox">
+                <h1><img className='logo' alt='logo' src={logo}/></h1>
                 <p id='registerText'>회원 가입</p>
                 <form className="register" onSubmit={onSubmitHandler}>
                     <label>아이디</label>
-                    <input className='input' placeholder="아이디를 입력하시오" type='id' value={Id} onChange={onIdHandler}/>
+                    <input className='input' placeholder="아이디를 입력하시오." type='id' value={Id} onChange={onIdHandler}/>
                     <label>비밀번호</label>
-                    <input className='input' placeholder="비밀번호를 입력하시오" type='password' value={Password} onChange={onPasswordHandler}/>
+                    <input className='input' placeholder="비밀번호를 입력하시오." type='password' value={Password} onChange={onPasswordHandler}/>
                     <label>비밀번호 확인</label>
-                    <input className='input' placeholder="비밀번호를 다시 입력하시오" type='password' value={ConfirmPassword} onChange={onConfirmPasswordHandler}/>
+                    <input className='input' placeholder="비밀번호를 다시 입력하시오." type='password' value={ConfirmPassword} onChange={onConfirmPasswordHandler}/>
                     <br/>
                     <button formAction=''>
-                        Create Account
+                        <img alt="register" src={regiserBtn}/>
                     </button>
                 </form>
             </div>
