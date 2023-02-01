0145ff8d8556bda12d58e453e40671e64a6ef42b
정지윤 <samtori3849@gmail.com>
2023-02-01 10:30:28 +0900

    로그인 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
-import Home from './page/Home.js'
-import loginBtn from './login.png';
-import logo from './logo.png';
-import LoginErrorModal from './Modal/LoginErrorModal.js';
 import { useState } from "react";
+import LoginPage from "./page/LoginPage.js";
 
 function App() {
 
@@ -11,20 +8,7 @@ function App() {
 
   return (
     <div className='App'>   
-      <div className='mainBox'>
-      <h1 className='logo'><img alt='logo' src={logo}/></h1><br/>
-        <p><text className='welcome'>환영합니다!</text></p><br/>
-        <p><input type="text" className='id' placeholder='아이디를 입력하세요.'></input></p>
-        <p><input className='id' type="password" placeholder='비밀번호를 입력하세요.'></input></p>
-        <br/>
-        <p><button className='loginBtn' onClick={()=>{
-          setModal(!modal)
-        }}><img alt="login" src={loginBtn}/></button></p>
-        <text className='joinBtn'>회원가입</text>
-      </div>
-      <text className='title'><h2><b>오늘의 투다</b></h2></text>
-      { modal === true ? <LoginErrorModal modal={modal} setModal={setModal}/> : null}
-      {/* <Home/> */}
+      <LoginPage/>
   </div>
   );
 }
---
+++ src/page/LoginPage.js
@@ -0,0 +1,30 @@
+import './LoginPage.css';
+import loginBtn from './login.png';
+import logo from './logo.png';
+import LoginErrorModal from '../Modal/LoginErrorModal.js';
+import { useState } from "react";
+
+
+export default function LoginPage(){
+
+    const [modal, setModal] = useState(false);
+
+    return (
+    <div className='Login'>   
+        <div className='mainBox'>
+        <h1 className='logo'><img alt='logo' src={logo}/></h1><br/>
+        <p><text className='welcome'>환영합니다!</text></p><br/>
+        <p><input type="text" className='id' placeholder='아이디를 입력하세요.'></input></p>
+        <p><input className='id' type="password" placeholder='비밀번호를 입력하세요.'></input></p>
+        <br/>
+        <p><button className='loginBtn' onClick={()=>{
+            setModal(!modal)
+        }}><img alt="login" src={loginBtn}/></button></p>
+        <text className='joinBtn'>회원가입</text>
+        </div>
+        <text className='title'><h2><b>오늘의 투다</b></h2></text>
+        { modal === true ? <LoginErrorModal modal={modal} setModal={setModal}/> : null}
+    </div>
+    )
+
+}
