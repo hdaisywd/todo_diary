3945a39c24acde9fc6be4fe66535c7ed8bf79c7e
정지윤 <samtori3849@gmail.com>
2023-02-01 11:13:05 +0900

    병합하게 해주세요

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
-import { useState } from "react";
 import LoginPage from "./page/LoginPage.js";
 
 function App() {
-
-  const [modal, setModal] = useState(false);
 
   return (
     <div className='App'>   
